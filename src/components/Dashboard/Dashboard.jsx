b51a0a36080fcadce162305202cbe61f8c9108ed
dipto230 <diptoroy230305@gmail.com>
2024-11-01 04:11:56 +0600

    navbar section done

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+
+
+const Dashboard = () => {
+    return (
+        <div>
+        h<h2>This our dashboard</h2>
+            
+        </div>
+    );
+};
+
+export default Dashboard;
--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,25 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './components/Root/Root';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import Home from './components/Home/Home';
+import Dashboard from './components/Dashboard/Dashboard';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'dashboard',
+        element:<Dashboard></Dashboard>
+      }
+    ]
   },
 ]);
 
