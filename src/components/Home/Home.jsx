cc0d38b559a01e1c75e130d32060a830f590fd91
dipto230 <diptoroy230305@gmail.com>
2024-11-01 05:11:15 +0600

    books detch data done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,14 @@
 
+import React from "react";
+import Banner from "../Banner/Banner";
+import Books from "../Books/Books";
+
 
 const Home = () => {
     return (
         <div>
-        <h2>This is our home page</h2>
+        <Banner></Banner>
+        <Books></Books>
             
         </div>
     );
