87ec5cce5992ff33f13acf5890c5b799213b9ca0
dipto230 <diptoroy230305@gmail.com>
2024-11-01 06:12:21 +0600

    card section fully done

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 
 
 const Book = ({book}) => {
-    const {image,bookName, author} =book;
+    const {bookId,image,bookName, author, tags, category} =book;
     return (
-        <div className="card bg-base-100 w-96 shadow-xl p-6">
+       
+       <Link to={`/books/${bookId}`}>
+          <div className="card bg-base-100 w-96 shadow-xl p-6">
         <figure className="bg-blue-100 py-8 rounded-2xl">
           <img
             src={image}
@@ -11,17 +14,33 @@ const Book = ({book}) => {
             alt={bookName} />
         </figure>
         <div className="card-body">
+         <div className="flex justify-center gap-4">
+         {
+            tags.map(tag=><button className="btn btn-xs bg-green-100">{tag}</button>)
+        }
+         </div>
           <h2 className="card-title">
             {bookName}
             <div className="badge badge-secondary">NEW</div>
           </h2>
           <p>By:{author}</p>
-          <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
+          <div className="divider"></div>
+          <div className="card-actions justify-between">
+            <div className="badge badge-outline">{category}</div>
+            
+            <div className="rating">
+  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" defaultChecked />
+  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+</div>
+            </div>
           </div>
         </div>
-      </div>
+       </Link>
+
+      
     );
 };
 
